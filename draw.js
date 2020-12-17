42f7cd18bfcd317e702cf6bd74a9a8fe91626920
adrs <ra.condezomonge@gmail.com>
2020-12-17 02:48:51 -0500

    Fix LineWeight.

--- draw.js
+++ draw.js
@@ -1,3 +1,4 @@
+var numberw = document.getElementById("number_weight");
 var btn_lw = document.getElementById("btn_lineWidth");
 var btn_clear = document.getElementById("clear");
 var teclas = {
@@ -9,13 +10,18 @@ var teclas = {
 
 btn_clear.addEventListener("click", f_clear);
 document.addEventListener("keydown", dibujarTeclado);
+btn_lw.addEventListener("click", fweight);
 
 var cuadro = document.getElementById("area_de_dibujo");
 var papel = cuadro.getContext("2d");
 var x = 150;
 var y = 150;
+var lw = 3;
 
 dibujarLinea("red", x - 1, y - 1, x + 1, y + 1, papel);
+function fweight() {
+  lw = parseInt(number_weight.value);
+}
 
 function f_clear() {
   cuadro.width = cuadro.width;
@@ -23,10 +29,10 @@ function f_clear() {
   y = 150;
 }
 
-function dibujarLinea(color, xin, yin, xfi, yfi, lienzo) {
+function dibujarLinea(color, xin, yin, xfi, yfi, lienzo, liw) {
   lienzo.beginPath();
   lienzo.strokeStyle = color;
-  lienzo.lineWidth = parseInt(btn_lw.value);
+  lienzo.lineWidth = liw;
   lienzo.moveTo(xin, yin);
   lienzo.lineTo(xfi, yfi);
   lienzo.stroke();
@@ -39,19 +45,19 @@ function dibujarTeclado(evento) {
 
   switch (evento.keyCode) {
     case teclas.UP:
-      dibujarLinea(color2, x, y, x, y - movimiemto, papel);
+      dibujarLinea(color2, x, y, x, y - movimiemto, papel, lw);
       y = y - movimiemto;
       break;
     case teclas.DOWN:
-      dibujarLinea(color2, x, y, x, y + movimiemto, papel);
+      dibujarLinea(color2, x, y, x, y + movimiemto, papel, lw);
       y = y + movimiemto;
       break;
     case teclas.LEFT:
-      dibujarLinea(color2, x, y, x - movimiemto, y, papel);
+      dibujarLinea(color2, x, y, x - movimiemto, y, papel, lw);
       x = x - movimiemto;
       break;
     case teclas.RIGHT:
-      dibujarLinea(color2, x, y, x + movimiemto, y, papel);
+      dibujarLinea(color2, x, y, x + movimiemto, y, papel, lw);
       x = x + movimiemto;
       break;
     default:
